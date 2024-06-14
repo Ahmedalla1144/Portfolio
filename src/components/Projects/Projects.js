fc4ab73c523a637250eef510a2fd9a5fa5a5b8fa
Ahmed Alaa <ahmed.alla56756@gmail.com>
2024-06-14 15:07:09 +0300

    editing the homepage for showing projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,5 +1,5 @@
-"use client"
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 import {
   BlogCard,
@@ -19,38 +19,60 @@ import {
   SectionDivider,
   SectionTitle,
 } from "../../styles/GlobalComponents";
+import axios from "axios";
 
-const Projects = ({projects}) => (
-  <Section nopadding >
-    <SectionDivider />
-    <SectionTitle main>Projects</SectionTitle>
-    <GridContainer>
-      {projects && projects.map(
-        ({image, title, description, tags, source, visit }, index) => (
-          <BlogCard key={index}>
-            <Img src={image} />
-            <TitleContent>
-              <HeaderThree title>{title}</HeaderThree>
-              <Hr />
-            </TitleContent>
-            <CardInfo>{description}</CardInfo>
-            <div>
-              <TitleContent>Stack</TitleContent>
-              <TagList>
-                {tags.map((tag, i) => (
-                  <Tag key={i}>{tag}</Tag>
-                ))}
-              </TagList>
-            </div>
-            <UtilityList>
-              <ExternalLinks target="_blank" href={source}>Code</ExternalLinks>
-              <ExternalLinks target="_blank" href={visit}>Visit</ExternalLinks>
-            </UtilityList>
-          </BlogCard>
-        )
-      )}
-    </GridContainer>
-  </Section>
-);
+export default function Projects() {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    async function getAllProjects() {
+      try {
+        const res = await axios.get("http://localhost:8000/api/projects");
+        if (res.data) {
+          setProjects(res.data);
+          console.log(res.data);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
 
-export default Projects;
+    getAllProjects();
+  }, []);
+  return (
+    <Section nopadding>
+      <SectionDivider />
+      <SectionTitle main>Projects</SectionTitle>
+      <GridContainer>
+        {projects &&
+          projects.map(
+            ({ image, title, description, tags, source, visit }, index) => (
+              <BlogCard key={index}>
+                <Img src={image} />
+                <TitleContent>
+                  <HeaderThree title>{title}</HeaderThree>
+                  <Hr />
+                </TitleContent>
+                <CardInfo>{description}</CardInfo>
+                <div>
+                  <TitleContent>Stack</TitleContent>
+                  <TagList>
+                    {tags.map((tag, i) => (
+                      <Tag key={i}>{tag}</Tag>
+                    ))}
+                  </TagList>
+                </div>
+                <UtilityList>
+                  <ExternalLinks target="_blank" href={source}>
+                    Code
+                  </ExternalLinks>
+                  <ExternalLinks target="_blank" href={visit}>
+                    Visit
+                  </ExternalLinks>
+                </UtilityList>
+              </BlogCard>
+            )
+          )}
+      </GridContainer>
+    </Section>
+  );
+}
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -5,26 +5,10 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 
 export default function Project() {
-  const [projects, setProjects] = useState([]);
-  useEffect(() => {
-    async function getAllProjects() {
-      try {
-        const res = await axios.get("http://localhost:8000/api/projects");
-        if (res.data) {
-          setProjects(res.data);
-          console.log(res.data);
-        }
-      } catch (error) {
-        console.error(error);
-      }
-    }
-
-    getAllProjects();
-  }, []);
   return (
     <div>
       <Layout>
-        <Projects projects={projects} />
+        <Projects />
       </Layout>
     </div>
   );
